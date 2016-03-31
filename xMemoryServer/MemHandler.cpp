3ca950345e7b0aead85c6685580019c948270695
ellioth <ellioth@ubuntu>
2016-03-31 15:48:14 -0600

    pasar datos a disco esta casi listo, falta pruebas y seguir con lo que seria la escritura en memoria

--- xMemoryServer/MemHandler.cpp
+++ xMemoryServer/MemHandler.cpp
@@ -5,6 +5,8 @@
  * Created on March 22, 2016, 9:10 PM
  */
 
+#include <string.h>
+
 #include "MemHandler.h"
 
 /**
@@ -13,10 +15,11 @@
  * @param port dato tipo entero, es el puerto donde crearemos
  * el server.
  */
-MemHandler::MemHandler(int port) {
+MemHandler::MemHandler(int port, char* DiskLocation) {
     _servidor= new servidor(port);
     _chuckMemory=malloc(SPACE_MEMORY);
-    _diskLocation=(char*)DISK_LOCATION;
+    _writerMemoryPointer=_chuckMemory;
+    _diskLocation=DiskLocation;
     _listaDatosAlmacenados= new lista();
 }
 
@@ -32,5 +35,39 @@ MemHandler::~MemHandler() {
  * que se a pedido.
  */
 void MemHandler::LoopForService() {
+    char* IncommingMessage= _servidor->listenMsg();
+    string getMsgDatas= IncommingMessage;
+    if(getMsgDatas.length()>=_MemoryLeft){
+        //pasamos toda los datos a disco.
+    }
+}
 
+void MemHandler::PassToDisk() {
+    fstream diskWriter(_diskLocation);
+    if(diskWriter.is_open()){
+        Nodo* temp=_listaDatosAlmacenados->getHead();
+        for(int i=0; i<_listaDatosAlmacenados->getSize(); i++){
+            if(!temp->saveAtDisk()){
+                //movmevos el puntero del archivo a la ultima posicion
+                diskWriter.seekg(_DiskPointer);
+                //obtenemos los datos del json que vamos a guardar
+                int space=temp->getSpaceSave();
+                int size=temp->getSizeSave();
+                //obtenemos el puntero de la memoria
+                void* PointerOfMemory=(_chuckMemory+space);
+                //escribimos en disco
+                diskWriter.write((char*)PointerOfMemory,size);
+                //alteramos los datos internos del nodo.
+                temp->setSaveDisk();
+                temp->setSpaceSave(_DiskPointer);
+                //movemos el puntero del disco
+                _DiskPointer+=size;
+            }
+            temp= temp->getNext();
+        }
+        //cerramos el archivo
+        diskWriter.close();
+    }
+    //limpiamos la memoria
+    bzero(_chuckMemory,DOSCIENTOS_CINCUENTA_Y_SEIS);
 }
--- xMemoryServer/MemHandler.h
+++ xMemoryServer/MemHandler.h
@@ -11,17 +11,23 @@
 #include "Lista.h"
 #include "Nodo.h"
 #include "servidor.h"
+#include <string>
+#include <fstream>
 
 class MemHandler :public Constantes {
 public:
-    MemHandler(int port);
+    MemHandler(int port, char* DiskLocation);
     virtual ~MemHandler();
     void LoopForService();
 private:
     servidor * _servidor;
     lista * _listaDatosAlmacenados;
     void* _chuckMemory;
+    void* _writerMemoryPointer;
     char* _diskLocation;
+    int _MemoryLeft;
+    int _DiskPointer;
+    void PassToDisk();
 };
 
 #endif	/* MEMHANDLER_H */
